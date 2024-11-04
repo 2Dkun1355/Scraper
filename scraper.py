f54abdb09b851aa4e1df56ded066005725e95f71
2Dkun1355 <oliynikbogdan.ua@gmail.com>
2024-11-04 20:57:57 +0200

    update scraper

--- scraper.py
+++ scraper.py
@@ -3,18 +3,24 @@ import re
 import sys
 import csv
 import json
-import random
 import asyncio
+from time import time
+from unicodedata import category
+from xml.etree import ElementTree
 
 import aiohttp
 from bs4 import BeautifulSoup
 from slugify import slugify
+import xmltodict
 
 
-class Scraper:
+class Scraper(object):
     BASE_URL = 'https://store.igefa.de'
     INTER_DATASET_PATH = 'intermediate_data.json'
     FINAL_DATASET_PATH = 'products.csv'
+    PAGES_URLS_PATH = 'pages_urls.txt'
+    PAGES_URLS_PROCESSED_PATH = 'pages_urls_processed.txt'
+    PRODUCT_URLS_PATH = 'products_urls.txt'
     CSV_FIELDNAMES = [
         'Product Name',
         'Original Data Column 1 (Breadcrumb)',
@@ -29,22 +35,18 @@ class Scraper:
         'Manufacturer',
         'Original Data Column 3 (Add. Description)',
     ]
-    DELAY = 0.01
+    DELAY = 0.1
 
     def __init__(self, *args, **kwargs):
         self._setup()
-        self.urls_to_scrape = []
+        self.category_index = 0
+        self.page_index = 0
+        self.product_index = 0
 
     def _setup(self, *args, **kwargs):
         if not os.path.isfile(self.INTER_DATASET_PATH):
             with open(self.INTER_DATASET_PATH, 'w') as f:
                 json.dump({}, f, indent=4)
-
-    @staticmethod
-    def _get_proxy(*args, **kwargs):
-        with open("proxies.txt") as f:
-            proxies = [line.strip() for line in f.readlines()]
-            return random.choice(proxies)
 
     @staticmethod
     def get_value(tag, *args, **kwargs):
@@ -53,58 +55,101 @@ class Scraper:
         except Exception:
             return None
 
-    async def scrape(self, session, *args, **kwargs):
-        """
-        Start scraping process.
-        Forms a list of products urls for scraping.
-        Run scraping products
-        """
+    async def scrape_categories(self, session, *args, **kwargs):
+        categories_xml_url = f'{self.BASE_URL}/sitemap-taxonomies.xml'
+        response = await session.get(categories_xml_url)
+        xml_string = await response.text()
+        xml_dict = xmltodict.parse(xml_string, process_namespaces=True)
+        categories_urls_dict = xml_dict.get('http://www.sitemaps.org/schemas/sitemap/0.9:urlset', {}).get('http://www.sitemaps.org/schemas/sitemap/0.9:url', [])
+        categories_urls = [d.get('http://www.sitemaps.org/schemas/sitemap/0.9:loc') for d in categories_urls_dict]
 
         tasks = []
-        for page_id in range(1, 5):  # count pages == 500
-            url = f'{self.BASE_URL}/c/kategorien/f4SXre6ovVohkGNrAvh3zR?page={page_id}'
-            task = asyncio.create_task(self.get_products_urls(session, url))
+        for category_url in categories_urls:
+            task = asyncio.create_task(self.get_page_urls(session, category_url))
             tasks.append(task)
             if self.DELAY:
                 await asyncio.sleep(self.DELAY)
-        urls_to_scrape = await asyncio.gather(*tasks, return_exceptions=True)
-        self.urls_to_scrape = [l for ls in urls_to_scrape for l in ls if isinstance(ls, set)]
-        print(f'* SCRAPED  {len(self.urls_to_scrape)} PRODUCT URLS')
-        await self.scrape_products(session)
 
-    async def get_products_urls(self, session, url: str, *args, **kwargs):
-        """ Return a list of products urls for scraping. """
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        page_urls = [l for ls in results if isinstance(ls, list) for l in ls]
+        with open(self.PAGES_URLS_PATH, 'w') as f:
+            f.write('\n'.join(page_urls))
 
-        urls = []
-        response = await session.get(url)
-        if response.status == 200:
+        return page_urls
+
+    async def get_page_urls(self, session, category_url, *args, **kwargs):
+        pages_urls = []
+        response = await session.get(category_url)
+        html = await response.text()
+        soup = BeautifulSoup(html, features='html.parser')
+        try:
+            json_data = soup.find('script', {'id': '__NEXT_DATA__'})
+            product_count = json.loads(json_data.text)['props']['initialProps']['pageProps']['initialProductData']['total']
+            page_count = product_count // 20 + 1 if product_count else 0
+        except KeyError:
+            page_count = 0
+
+        self.category_index += 1
+        print(f'*** SCRAPED CATEGORY {self.category_index}: {category_url} - {page_count}')
+        if page_count:
+            pages_urls = [f'{category_url}?page={page}' for page in range(1, page_count+1)]
+        return pages_urls
+
+    async def scrape_pages(self, session, pages_urls, *args, **kwargs):
+        urls = set(pages_urls) - set(self._get_processed_page_urls())
+        tasks = []
+        for page_urls in urls:
+            task = asyncio.create_task(self.get_product_urls(session, page_urls))
+            tasks.append(task)
+            if self.DELAY:
+                await asyncio.sleep(self.DELAY)
+
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        product_urls = [l for ls in results if isinstance(ls, list) for l in ls]
+        with open(self.PRODUCT_URLS_PATH, 'a') as f:
+            f.write('\n'.join(product_urls))
+
+        return product_urls
+
+    async def get_product_urls(self, session, page_url, *args, **kwargs):
+        product_urls = []
+        try:
+            response = await session.get(page_url)
             html = await response.text()
             items = re.findall(r'"mainVariant":\s*{.*?"id":"(.*?)".*?"slug":"(.*?)".*?}', html, re.DOTALL)
-            for product_id, slug in items:
-                url = f"{self.BASE_URL}/p/{slug}/{product_id}/"
-                urls.append(url)
-            urls_to_scrape = set(urls) - set(self._get_processed_urls())
-            return urls_to_scrape
-        return []
 
-    async def scrape_products(self, session, *args, **kwargs):
-        """ Scraping detail products urls. """
+            self.page_index += 1
+            print(f'*** SCRAPED PAGE: {self.page_index}: {page_url}')
 
+            if items:
+                self._save_processed_page_url(page_url)
+                for product_id, slug in items:
+                    url = f"{self.BASE_URL}/p/{slug}/{product_id}/"
+                    product_urls.append(url)
+            return product_urls
+
+        except Exception as e:
+            print(e, page_url)
+            return product_urls
+
+    async def scrape_products(self, session, products_urls, *args, **kwargs):
         tasks = []
-        for product_url in self.urls_to_scrape:
+        urls = set(products_urls) - set(self._get_processed_product_urls())
+        print(f'** RUN {len(urls)} PRODUCT URLS...')
+        for product_url in urls:
             task = asyncio.create_task(self.parse_product(session, product_url))
             tasks.append(task)
             if self.DELAY:
                 await asyncio.sleep(self.DELAY)
-        await asyncio.gather(*tasks, return_exceptions=True)
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        saved_products_count = sum(results)
+        return saved_products_count
 
-    async def parse_product(self, session, url: str, *args, **kwargs):
-        """ Parse product html and forms data """
-
+    async def parse_product(self, session, product_url: str, *args, **kwargs):
         data = {}
-        response = await session.get(url)
-        if response.status == 200:
-            try:
+        try:
+            response = await session.get(product_url)
+            if response.status == 200:
                 html = await response.text()
                 soup = BeautifulSoup(html, features='html.parser')
 
@@ -143,25 +188,46 @@ class Scraper:
                     'Article Number': article_number,
                     'Product Description': product_description,
                     'Supplier': supplier,
-                    'Supplier-URL': url,
+                    'Supplier-URL': product_url,
                     'Product Image URL': product_image,
                     'Manufacturer': manufacturer,
                     'Original Data Column 3 (Add. Description)': original_data_column,
                 })
-                self._save_product_to_inter_dataset(url, data)
+                self._save_product_to_inter_dataset(product_url, data)
+                return 1
+        except Exception as e:
+            print(product_url)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(f"Error: {exc_type}, {fname}, {exc_tb.tb_lineno}!")
+            return 0
 
-            except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                # print(f"Error: {exc_type}, {fname}, {exc_tb.tb_lineno}!")
+    def _get_processed_page_urls(self, *args, **kwargs):
+        try:
+            with open(self.PAGES_URLS_PROCESSED_PATH, 'r') as f:
+                return [line.strip() for line in f.readlines()]
+        except:
+            return []
 
-    def _get_processed_urls(self, *args, **kwargs):
+    def _get_processed_product_urls(self, *args, **kwargs):
         with open(self.INTER_DATASET_PATH, 'r') as f:
-            processed_urls = json.load(f).keys()
-            return processed_urls
+            return json.load(f).keys()
+
+    def _save_processed_page_url(self, page_url, *args, **kwargs):
+        with open(self.PAGES_URLS_PROCESSED_PATH, 'a') as f:
+            f.write(f'{page_url}\n')
+
+    def _get_pages_urls_from_file(self, *args, **kwargs):
+        with open(self.PAGES_URLS_PATH, 'r') as f:
+            return [line.strip() for line in f.readlines()]
+
+    def _get_product_urls_from_file(self, *args, **kwargs):
+        with open(self.PRODUCT_URLS_PATH, 'r') as f:
+            return [line.strip() for line in f.readlines()]
 
     def _save_product_to_inter_dataset(self, url: str, data: dict, *args, **kwargs):
         """ Save product data to intermediate json dataset """
+        self.product_index += 1
 
         with open(self.INTER_DATASET_PATH, 'r') as fr:
             json_data = json.load(fr)
@@ -169,10 +235,10 @@ class Scraper:
         with open(self.INTER_DATASET_PATH, 'w') as fw:
             json_data.update({url: data})
             json.dump(json_data, fw, ensure_ascii=False, indent=4)
-            print(f'* SAVED  {url} TO INTERMEDIATE DATASET')
+            print(f'* SAVED {self.product_index} - {url} - TO INTERMEDIATE DATASET')
 
     def create_final_dataset(self, *args, **kwargs):
-        """ Create csv dataset from intermadiate json dataset. """
+        """ Create csv dataset from intermediate json dataset. """
 
         with open(self.INTER_DATASET_PATH, 'r') as fr:
             json_data = json.load(fr)
@@ -182,15 +248,32 @@ class Scraper:
             writer.writeheader()
             for product_data in json_data.values():
                 writer.writerow(product_data)
-            print(f'* SAVED FINAL DATASET')
-
 
     async def run(self, *args, **kwargs):
         async with aiohttp.ClientSession() as session:
-            print('* START SCRAPING')
-            await self.scrape(session)
-        self.create_final_dataset()
+            print('* START SCRAPING...')
+            t1 = time()
 
+            # 1. Scrape categories form xml and get list of page_url (12400 pages - 300sec)
+            # pages_urls = await self.scrape_categories(session)
+            pages_urls = self._get_pages_urls_from_file()
+            print(f'* SCRAPED: {len(pages_urls)} PAGES URLS.')
+
+            # 2. Scrape page_url and get list or product_url( 76000 product unique )
+            # products_urls = await self.scrape_pages(session, pages_urls)
+            products_urls = self._get_product_urls_from_file()
+            print(f'* SCRAPED: {len(products_urls)} PRODUCTS URLS.')
+
+            # 3. Scrape product_urls, parse data and save to inter dataset
+            saved_count = await self.scrape_products(session, products_urls)
+            print(f'* SAVED: {saved_count} PRODUCTS TO INTER DATASET.')
+
+            # 4. Save final dataset form inter dataset
+            self.create_final_dataset()
+            print(f'* SAVED FINAL DATASET.')
+            t2 = time()
+            print(f'* TIME: {t2-t1}')
+            print('* FINISH SCRAPING PRODUCTS')
 
 
 
